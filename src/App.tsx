66856cf63b8fc84d4fec2be2c79f5056fd015fec
deljus <musindelus@gmail.com>
2026-04-30 20:11:47 +0300

    kkkk

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,7 @@
 import { ScrollBar } from "./components/scroll-bar";
 import { Hero } from "./components/hero";
-import { useEffect, useState } from "react";
 
 function App() {
-  const [count, setCount] = useState(0);
-  console.log("1");
-  useEffect(() => {
-    console.log("2", count);
-  }, [count]);
-
   return (
     <div className="container mx-auto">
       <ScrollBar />
